42b1334233732f5b809a6dd9c99cc50bb23f5a92
Gaffyn Voorzta <78822476+gaffynvoorzta@users.noreply.github.com>
2021-08-11 19:03:05 +0100

    Update settings.py
    protect my vitals

--- djangodelights/settings.py
+++ djangodelights/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-%ii3)&0-4ffgf_4ul7a&k9%*at#6*)5_7fersvas0-_wd=n87^'
+SECRET_KEY = 'ssshhhhh'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
